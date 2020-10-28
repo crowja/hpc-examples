13ee7cbca29052a390541d6d09ea1aa64b3cd7da
John Crow <johna.crow@corteva.com>
2020-10-27 20:10:58 -0500

    Initial commit.

---
+++ python/pools01.py
@@ -0,0 +1,39 @@
+#! /usr/bin/env python3
+
+from multiprocessing import Pool, TimeoutError
+import os
+import time
+
+
+class Task:
+    def __init__(self):
+        self.i = False
+        self.j = False
+
+    def set_params(self, i, j):
+        self.i = i
+        self.j = j
+
+
+def analysis(x):
+    print("==")
+    print(f"working in mypid={os.getpid()} with vals {x.i} and {x.j}")
+    time.sleep(3)
+    return os.getpid()
+
+
+if __name__ == "__main__":
+
+    # Create a task list, each task object representing a set of parameters
+    # to use by analysis() above.
+    tasks = []
+    for i in range(10):
+        task = Task()
+        task.set_params(i, i + 1)
+        tasks.append(task)
+
+    # Set up a pool of processes and let the pool work through the task list.
+    ##with Pool(processes=2) as pool:
+    with Pool() as pool:
+        blah = pool.map(analysis, tasks)
+        print(blah)
